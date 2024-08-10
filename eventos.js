11e50c2176882cddb0e8931b68018d2285115bc3
Fernanda3172005 <ferchucosta2005@gmail.com>
2024-08-10 14:53:05 -0300

    solucion

--- eventos.js
+++ eventos.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let div = document.getElementById("button");
+    let div = document.getElementById("div");
+    let boton = document.getElementById("button");
 
+    // Manejador para el div
     div.addEventListener("click", function() {
         alert("Hola! Soy el div");
+    });
+
+    // Manejador para el botón
+    boton.addEventListener("click", function(event) {
+        event.stopPropagation(); // Detiene la propagación del evento hacia el div
+        alert("Hola! Soy el botón");
     });
 });
