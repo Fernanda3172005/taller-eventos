d0a14116db1c19dfccf1a58de67fcd1d9dc895d8
Fernanda3172005 <ferchucosta2005@gmail.com>
2024-08-10 14:20:43 -0300

    Style y script en div de butoon

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let div = document.getElementById("button");
+
+    div.addEventListener("click", function() {
+        alert("Hola! Soy el div");
+    });
+});
